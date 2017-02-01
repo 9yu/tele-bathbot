abfa73efc2a1b384479811fd8ff2570cbde1f8e4
9yu <9.yubako@gmail.com>
2017-02-01 20:08:17 +0800

    i have no idea

--- bath.php
+++ bath.php
@@ -37,8 +37,8 @@ if(!is_null($text) && !is_null($chat_id)){
 		$url = "http://api.hitokoto.cn/";
 		$json = file_get_contents($url);
 		$de_json = json_decode($json, true);
-		$text = de_json['hitokoto'] + "  ——" + de_json['from'];
-		$content = array('chat_id' => $chat_id, 'text' => $text);
+		//$text = de_json['hitokoto'] + "  ——" + de_json['from'];
+		$content = array('chat_id' => $chat_id, 'text' => $de_json['hitokoto']);
 		$telegram->sendMessage($content);
 	}
 
