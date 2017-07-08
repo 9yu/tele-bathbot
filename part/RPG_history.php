8dc97aed867f65f950a2d1bfb44685004213c12b
9yu <9.yubako@gmail.com>
2017-07-08 17:57:21 +0800

    1757

--- part/RPG_history.php
+++ part/RPG_history.php
@@ -2,35 +2,29 @@
 
 if( file_exists("data.history.$username.cache.json") )
 {
-	// 有历史记录
-	/**
 	$history = json_decode( file_get_contents("data.history.$username.cache.json"), true);
-	// 保存 16 个数据
-	if ( count($history) > 8 )
+	if( count($history) >= 8 )
 	{
-		// 1 - 7
-		$return_all = $history[count($history) - 1] . "\n …… \n" . $history[6] . "\n" . $history[5] . "\n" . $history[4] . "\n" . $history[3] . "\n" . $history[2] . "\n" . $history[1] . "\n" . $history[0] . "\n * " . $return_text;
+		// 0 -7
+		$return_all = "";
+		for ( $i = 7 ; $i >= 0; $i -- )  
+		{ 
+			$return_all .= $history[$i] . "\n";
+		}
+		$return_all .= " * " . $return_text;
 	}
 	else
-	{   // 最多 7 个
-		$return_all = "";
-		//for ( $i = count($history) - 1; $i > -1; $i -- ) { 
-		//	$return_all .= $history[$i] . "\n"; 
-		//}
-		$return_all = $return_all . ' * ' . $return_text;
-	}
-
-	// 抛弃旧记录
-	if( count($history) = 16 )
 	{
-		unset($history[15]);
+		$return_all = "";
+		for ( $i = count($history) - 1;  $i >= 0 ;  $i -- ) 
+		{ 
+			$return_all .= $history[$i] . "\n";
+		}
+		$return_all .= " * " . $return_text;
 	}
 
-	// 写入 json
 	array_unshift($history, $return_text);
-	**/
-	$return_all = $return_text;
-	$history = array($return_text);
+
 }
 else
 {
